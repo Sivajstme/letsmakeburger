7990313d4dfe3c1af21778d4dbf2b1eb46827b22
siva <shivajstme@gmail.com>
2021-07-01 14:17:21 +1000

    Added nav

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Aux from '../../hoc/Aux';
 import './Layout.css';
-
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 const layout = (props) =>{
     
     return(
         <Aux>
-            <div>Toolbar, sideDrawer, backdrop</div>
+            {/* <div>Toolbar, sideDrawer, backdrop</div> */}
+            <Toolbar />
             <main className='Content'>{props.children}</main>
         </Aux>
     )
---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './Toolbar.css'
+
+const toolbar = ()=> {
+
+    return(
+        <header className='Toolbar'>
+            <div>MENU</div>
+            <div>LOGO</div>
+            <nav>
+                ...
+            </nav>
+
+        </header>
+    )
+
+}
+
+export default toolbar;
